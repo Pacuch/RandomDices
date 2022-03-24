cd01e83febadd1bc7da40381e9bd8329ad6ab06c
Pacuch <michal.patz@interia.pl>
2022-03-24 16:36:14 +0100

    RandomDices

---
+++ app/src/main/java/com/example/firstcubes/Dice.kt
@@ -0,0 +1,16 @@
+package com.example.firstcubes
+
+import java.util.*
+
+class Dice (val numSides: Int = 6){
+    fun roll(isRangesRandom: Boolean = true): Int {
+        if(isRangesRandom){
+            return (1..numSides).random()
+
+        }
+        else{
+            return Random().nextInt(numSides) + 1
+        }
+
+    }
+}
